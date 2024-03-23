5ac3e1ac4b8ecc22de34a19851786d534f0c5edb
Cory Thomas Bandrowsky <corythomasbandrowsky@gmail.com>
2024-03-23 16:29:53 -0400

    add users controller wip

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using RubberCity.Services.Services;
 using RubberCityAPI.Models;
 using System.Text.Json;
 using System.Text.Json.Nodes;
@@ -9,11 +12,20 @@ namespace RubberCityAPI.Controllers
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
+        #region members
         private readonly ILogger<HomeController> _logger;
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppSettings _appSettings;
+        #endregion members
+        #region controller
+        public HomeController(IOptionsSnapshot<AppSettings> appSettings,
+            ILogger<HomeController> logger,
+            UserService userService)
         {
+            _appSettings = appSettings.Value;
             _logger = logger;
         }
+        #endregion controller
+        #region functions
 
         [HttpGet]
         public async Task<IActionResult> Index()
@@ -21,5 +33,6 @@ namespace RubberCityAPI.Controllers
             var response = new TestResponse("dad time");
             return Ok(response);
         }
+        #endregion functions
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,33 @@
+using Domain.Models;
+using Microsoft.Extensions.Configuration;
+using RubberCity.Data.Interfaces;
+using RubberCity.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
 var allowOrigins = "_allowOrigins";
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: allowOrigins,
                       builder =>
                       {
-                          builder.WithOrigins("http://localhost:4200/home",
-                                              "http://localhost:4200");
+                          var corsOrigins = configuration.GetSection("CorsOrigins").Get<string[]>();
+                          builder.WithOrigins(corsOrigins)
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
                       });
 });
 
+var connectionString = configuration.GetConnectionString("RubberCityMaster");
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("Values"));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository<User>, UserRepository>(sp =>
+    new UserRepository(builder.Configuration.GetConnectionString("RubberCityMaster")));
 
 var app = builder.Build();
 app.UseHttpsRedirection();
@@ -22,7 +37,6 @@ app.UseCors(allowOrigins);
 app.UseAuthorization();
 app.MapControllers();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
